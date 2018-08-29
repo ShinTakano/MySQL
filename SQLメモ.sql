2fe2e7ae60bf4dc8b796c7f65c44d8a7202c379f
ShinTakano <40346849+ShinTakano@users.noreply.github.com>
2018-08-29 18:51:42 +0900

    sql課題終了

--- SQLメモ.sql
+++ SQLメモ.sql
@@ -1,4 +1,4 @@
-・CREATE DATABESE rezodb DEFAULT CHARACTER SET utf8;/データベース新規作成「CREATE DATABESE データベース名;」
+・CREATE DATABASE rezodb DEFAULT CHARACTER SET utf8;/データベース新規作成「CREATE DATABESE データベース名;」
 ・USE rezodb;/データベース使用「USE データベース名;」
 ・CREATE TABLE user(user_no int,user_name varchar(30),position varchar(10),mail varchar(50);
 /テーブル新規作成「CREATE TABLE テーブル名(カラム名1,データ型1,....;」
@@ -16,5 +16,5 @@
 ・DELETE FROM user WHERE user_no = 10005;/データの削除「DELETE FROM テーブル名 WHERE カラム名 = 削除したい名称;」
 UNIQUE = 同じ名称は使うなという命令
 NOT NULL = 必ず何か入れなければいけないという命令
-PRIMARY KEY = UNIQUE+NOT NULL を合わせた命令
+PRIMARY KEY = UNIQUE+NOT NULL を合わせた命令(主キーと呼ばれる）
 AUTO_INCREMENT = 自動で番号を追加する命令
